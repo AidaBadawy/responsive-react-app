9eaf4f7a19644eb9c9c7666250fb739eae48ff88
Aidarus Badawy <aidarusbadawy@gmail.com>
2021-08-16 22:46:38 +0300

    Adding Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,38 @@
+import {useState} from 'react'
+import logo from '../images/logo.png'
 
 function Navbar() {
+    const [nav , setNav] = useState(false);
+
+    const changeBackground = () => {
+        if(window.scrollY >= 50){
+            setNav(true);
+        }
+        else {
+            setNav(false);
+        }
+    }
+    window.addEventListener('scroll', changeBackground)
     return (
-        <div>
+        <nav className={nav ? 'nav active' : 'nav'}>
+            <a href='#' className='logo'>
+                <img src={logo} alt='logo'/>
+            </a>
+            <input type='checkbox' className='menu-btn' id='menu-btn'/>
+            <label className='menu-icon' for='menu-btn'>
+                <span className='nav-icon'></span>
+            </label>
+            <ul className='menu'>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Features</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>UI SS</a></li>
+                <li><a href='#'>Download</a></li>
+
+
+            </ul>
             
-        </div>
+        </nav>
     )
 }
 
